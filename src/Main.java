1707cd7d8b118dd6ee40b2071fc98b42d193c519
Damian <dambrz97@gmail.com>
2019-06-19 14:54:24 +0200

    Resolve merge conflicts
    Merged branch: feature/calculator

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,20 @@ public class Main {
         User user = new User(name, lastname, age);
         System.out.println("Create user: " + user.toString());
     }
+    public static void calculate() {
+        Integer x, y;
+        System.out.println("Type first number: ");
+        x = scanner.nextInt();
+        System.out.println("Type second number: ");
+        y = scanner.nextInt();
+        System.out.println("Sum: " + Calculator.add(x, y));
+        System.out.println("Subtract: " + Calculator.subtract(x, y));
+        System.out.println("Multiply: " + Calculator.multiply(x, y));
+        System.out.println("Divide: " + Calculator.divide(x, y));
+    }
 
     public static void main(String[] args) {
         createuser();
+        calculate();
     }
 }
