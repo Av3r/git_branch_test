c036941709cf1ae014fdc34224a440a0cb8fce51
Damian <dambrz97@gmail.com>
2019-06-19 14:39:41 +0200

    Create calculator class
    Class contains methods: add/substract/multiply/divide

---
+++ scr/src/Calculator.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Damia on 19.06.2019.
+ */
+public class Calculator {
+    public static Integer add(Integer numberOne, Integer numberTwo) {
+        return numberOne + numberTwo;
+    }
+    public static Integer subtract(Integer numberOne, Integer numberTwo) {
+        return numberOne - numberTwo;
+    }
+    public static Integer multiply(Integer numberOne, Integer numberTwo) {
+        return numberOne * numberTwo;
+    }
+    public static Float divide(Integer numberOne, Integer numberTwo) {
+        return numberOne / (float) numberTwo;
+    }
+}
